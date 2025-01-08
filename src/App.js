4e65f7433bb94b6c0d7c45261d7359798a6c65d7
vio3500 <vio3500@github>
2025-01-08 17:14:23 +0800

    Upload initial web

--- src/App.js
+++ src/App.js
@@ -3,12 +3,25 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <div className="moon-container">
-          <div className="moon"></div>
+      <div className="App">
+          <div className="hs-artifacts">
+              <div className="vertical-line"></div>
+              <div className="vertical-text">
+                  HIGH SCHOOL<br/>ARTIFACTS
+              </div>
+          </div>
+          <div className="element-container">
+              <div className="moon"></div>
+              <label className="switch">
+                  <input className="cb" type="checkbox"/>
+                  <div className="toggle">
+                      <div className="left"><span className="text">off</span></div>
+                      <div className="right"><span className="text">on</span></div>
+                  </div>
+              </label>
+          </div>
+          <div className="title"></div>
       </div>
-        <div className="title"></div>
-    </div>
   );
 }
 
